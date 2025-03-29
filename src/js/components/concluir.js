525a6a718cabc18cdcc5d428e067446e0a17b9cd
oKelvinCosta <okelvincosta@gmail.com>
2025-03-29 00:13:55 -0300

    Commit inicial

---
+++ src/js/components/concluir.js
@@ -0,0 +1,16 @@
+export default {
+  mounted(){
+console.log('oi');
+
+  },
+    template: //html
+    `
+    <div id="concluir" class="py-144 scrollspy flex--justify-center" >
+       
+        <a  href="#/" id="finishButton" class="btn-large filled waves-effect waves-light bubbly-button">Concluir
+          <span class="ml-8 pb-8 material-symbols-rounded"> check </span>
+        </a>
+        
+    </div>
+    `,
+  };
